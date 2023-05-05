f51b8479cf031ad6e52e0ae77938bfff544d92df
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-05-05 21:52:50 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,28 @@ import Image from "next/image";
 import { Karla } from "next/font/google";
 import Navbar from "../components/Navbar/Navbar";
 import Questions from "../components/Questions/Questions";
+import Head from "next/head";
 
 const karla = Karla({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`${karla.className} px-3 max-w-[1440px] mx-auto
+    <>
+      <Head>
+        <title>Solo Project - Tier 2 - Chingu Trivia</title>
+        <meta
+          name="description"
+          content="Chingu Trivia, solo project, questions included HTML, CSS and javascript category"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main
+        className={`${karla.className} px-3 max-w-[1440px] mx-auto
     sm:px-7 sm:pt-5`}
-    >
-      <Navbar />
-      <Questions />
-    </main>
+      >
+        <Navbar />
+        <Questions />
+      </main>
+    </>
   );
 }
